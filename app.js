a7d8e5cb444b3e36a69701754b5740df7aee86f9
imdara168 <lungsdara@gmail.com>
2024-03-24 09:22:45 +0700

    fix adding number

---
+++ app.js
@@ -0,0 +1,24 @@
+function sum() {
+  name = "Sovandara";
+  return 10 + 10;
+}
+const arr = [30];
+console.log(sum() - arr[0]);
+
+const Array = [
+  { id: 1, name: "sovandara" },
+  { id: 2, name: "sovandara" },
+  { id: 3, name: "sovandara" },
+  { id: 4, name: "sovandara" },
+];
+
+console.log(Array[3].name);
+
+const obj = {
+  name: "Sovandara",
+  arr1: [20],
+  num: 50,
+  num1: ,
+};
+
+console.log(obj.num1);
